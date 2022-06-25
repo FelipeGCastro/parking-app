3116d5d2ec84ddc07d60c2a3a00588b25647e12f
Luiz Felipe Castro <felipe1693@gmail.com>
2022-06-25 14:10:39 +0100

    check env variable

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,12 +2,12 @@ import axios from 'axios'
 import Constants from 'expo-constants'
 const { manifest } = Constants
 
-const base =
-  typeof manifest.packagerOpts === `object` && manifest.packagerOpts.dev
-    ? `http://${manifest.debuggerHost.split(`:`).shift().concat(`:3000`)}`
-    : process.env.BASE_URL
+const base = process.env.BASE_URL
+// typeof manifest.packagerOpts === `object` && manifest.packagerOpts.dev
+//   ? `http://${manifest.debuggerHost.split(`:`).shift().concat(`:3000`)}`
+//   : process.env.BASE_URL
 
 export const api = axios.create({
   // baseURL: base,
-  baseURL: 'https://parking-node-backend.herokuapp.com/',
+  baseURL: base,
 })
