9e482408dc683a2fe058ee79c773cbd448aef3a8
Luiz Felipe Castro <felipe1693@gmail.com>
2022-07-04 01:17:36 +0100

    change order of navigator

--- src/navigation/MainNavigator/index.tsx
+++ src/navigation/MainNavigator/index.tsx
@@ -1,24 +1,16 @@
-
 import { NavigationContainer } from '@react-navigation/native'
-import { createStackNavigator } from '@react-navigation/stack';
-import { HomeNavigator } from '../HomeNavigator';
-import { OnBoardingNavigator } from '../OnboardingNavigator';
+import { createStackNavigator } from '@react-navigation/stack'
+import { HomeNavigator } from '../HomeNavigator'
+import { OnBoardingNavigator } from '../OnboardingNavigator'
 
-const Stack = createStackNavigator();
+const Stack = createStackNavigator()
 
 export const MainNavigator = () => {
-
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerShown: false }} >
-        <Stack.Screen
-          name="OnBoarding"
-          component={OnBoardingNavigator}
-        />
-        <Stack.Screen
-          name="Home"
-          component={HomeNavigator}
-        />
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Home" component={HomeNavigator} />
+        <Stack.Screen name="OnBoarding" component={OnBoardingNavigator} />
       </Stack.Navigator>
     </NavigationContainer>
   )
